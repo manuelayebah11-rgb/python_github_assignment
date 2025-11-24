15a97c7cf97a9e3e65a0bc03345fa329ab1a199b
manuelayebah11-rgb <manuelayebah11@gmail.com>
2025-11-23 21:06:05 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-print("Welcome to my Python program!")
-hours = input("How many hours did you study today? ")
+print("Welcome to my Python program!") #Welcom message
+hours = input("How many hours did you study today? ") #Ask the user for how many hours they study
 hours = float(hours)
 weekly_hours = hours * 7
-print(f"You are on track to study {weekly_hours} hours this week.")
-try:
+print(f"You are on track to study {weekly_hours} hours this week.") # Calculate weekly study hours
+try: # Convert input to float, with error handling
     hours = float(hours)
 except ValueError:
     print("Please enter a valid number.")
